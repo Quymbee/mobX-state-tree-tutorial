56c0059ca32aa778d285c1306b6552f766c1078f
Quymbee Chen <quymbee@mattchen.com>
2020-12-15 11:44:01 -0500

    first 4 videos

--- src/models/WishList.js
+++ src/models/WishList.js
@@ -33,4 +33,9 @@ export const WishList = types
     add(item) {
       self.items.push(item);
     },
+  }))
+  .views((self) => ({
+    get totalPrice() {
+      return self.items.reduce((sum, entry) => sum + entry.price, 0);
+    },
   }));
--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -1,3 +1,5 @@
+import { getSnapshot, onSnapshot, onPatch } from "mobx-state-tree";
+import { reaction } from "mobx";
 import { WishListItem, WishList } from "./WishList";
 
 it("can create an instance of a model", () => {
@@ -48,16 +50,90 @@ it("can create a wishlist", () => {
 
 it("can add new items", () => {
   const list = WishList.create();
-  list.add(
-    WishListItem.create({
-      name: "Chesterton",
-      price: 10,
-    })
-  );
+  const states = [];
+  onSnapshot(list, (snapshot) => {
+    states.push(snapshot);
+  });
+
+  list.add({
+    name: "Chesterton",
+    price: 10,
+  });
 
   expect(list.items.length).toBe(1);
   expect(list.items[0].name).toBe("Chesterton");
 
   list.items[0].changeName("Book of Chesterton");
   expect(list.items[0].name).toBe("Book of Chesterton");
+
+  /*
+   with use of getSnapshot(), 
+   you can have it match against immutable snapshot object
+  */
+  expect(getSnapshot(list)).toEqual({
+    items: [
+      {
+        name: "Book of Chesterton",
+        price: 10,
+        image: "",
+      },
+    ],
+  });
+
+  //jest allows toMatchSnapshot()
+  expect(getSnapshot(list)).toMatchSnapshot();
+
+  //uses states array instead of getSnapshot(list) that detects any change
+  expect(states).toMatchSnapshot();
+});
+
+it("can add new items - 2", () => {
+  const list = WishList.create();
+  const patches = [];
+  onPatch(list, (snapshot) => {
+    patches.push(snapshot);
+  });
+
+  list.add({
+    name: "Chesterton",
+    price: 10,
+  });
+
+  list.items[0].changeName("Book of Chesterton");
+
+  expect(patches).toMatchSnapshot();
+});
+
+it("can calculate the total price of a wishlist", () => {
+  const list = WishList.create({
+    items: [
+      {
+        name: "Lipstick",
+        price: 7.35,
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/51ZeZZD1XML._SX258_BO1,204,203,200_.jpg",
+      },
+      {
+        name: "Lego",
+        price: 27.35,
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/51ZeZZD1XML._SX258_BO1,204,203,200_.jpg",
+      },
+    ],
+  });
+
+  expect(list.totalPrice).toBe(34.7);
+
+  let changed = 0;
+  reaction(
+    () => list.totalPrice,
+    () => changed++
+  );
+
+  //shows that calculating total price isn't costly
+  expect(changed).toBe(0);
+  list.items[0].changeName("Test");
+  expect(changed).toBe(0);
+  list.items[0].changePrice(29);
+  expect(changed).toBe(1);
 });
